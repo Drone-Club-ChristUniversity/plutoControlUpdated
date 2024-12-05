0b11e0eb0f1e88c195baa26acc3bd4cbdc9954ec
Shrey <shreyjain1403@gmail.com>
2024-12-05 21:15:03 +0530

    just using normal versioning

--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
 from setuptools import setup, find_packages
-import subprocess
-import os
-
-def get_version():
-    result = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True)
-    version = result.stdout.strip()
-    if "-" in version:
-        # Convert to PEP 440 compliant version
-        v, i, s = version.split("-")
-        version = v + "+" + i + ".git." + s
-    return version
-
-pypluto_version = get_version()
-
-print("Version: ", pypluto_version)
-
-if not pypluto_version:
-    raise ValueError("Version could not be determined from git tags.")
-
-assert "-" not in pypluto_version, "Version format is incorrect"
-assert "." in pypluto_version, "Version format is incorrect"
-
-# if not os.path.isfile("pypluto/version.py"):
-#     raise FileNotFoundError("Required file 'pypluto/version.py' not found.")
-
-# with open("pypluto/VERSION", "w", encoding="utf-8") as fh:
-#     fh.write(f"{pypluto_version}\n")
-
 
 setup(
     name='pypluto-christ',
-    version=pypluto_version,
+    version="0.2.1",
     packages=find_packages(),
     install_requires=[],
     author='Shrey Jain',
