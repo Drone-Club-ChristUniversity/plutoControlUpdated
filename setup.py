66d9877c751b15ba7af5b8e4e78a712da52a50ab
Shrey <shreyjain1403@gmail.com>
2024-12-05 20:33:29 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -2,30 +2,32 @@ from setuptools import setup, find_packages
 import subprocess
 import os
 
-cf_remote_version = (
+pypluto_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
     .stdout.decode("utf-8")
     .strip()
 )
 
-if "-" in cf_remote_version:
+if "-" in pypluto_version:
     # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
     # pip has gotten strict with version numbers
     # so change it to: "1.3.3+22.git.gdf81228"
     # See: https://peps.python.org/pep-0440/#local-version-segments
-    v,i,s = cf_remote_version.split("-")
-    cf_remote_version = v + "+" + i + ".git." + s
+    v,i,s = pypluto_version.split("-")
+    pypluto_version = v + "+" + i + ".git." + s
 
-assert "-" not in cf_remote_version
-assert "." in cf_remote_version
+assert "-" not in pypluto_version
+assert "." in pypluto_version
 
-assert os.path.isfile("cf_remote/version.py")
-with open("cf_remote/VERSION", "w", encoding="utf-8") as fh:
-    fh.write("%s\n" % cf_remote_version)
+assert os.path.isfile("pypluto/version.py")
+with open("pypluto/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % pypluto_version)
+
+print("Version: ", pypluto_version)
 
 setup(
     name='pypluto-christ',
-    version=cf_remote_version,
+    version=pypluto_version,
     packages=find_packages(),
     install_requires=[],
     author='Shrey Jain',
